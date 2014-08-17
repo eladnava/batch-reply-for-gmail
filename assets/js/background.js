95c4a34db8cfcb3660e58b472d59b06534b5632f
Elad Nava <eladnava@gmail.com>
2014-08-18 00:53:54 +0300

    First commit

---
+++ assets/js/background.js
@@ -0,0 +1,62 @@
+//----------------------------
+// Limit listeners to
+// https://mail.google.com/mail
+//----------------------------
+
+var filter =
+{
+    'url': [
+        {
+            hostSuffix: 'google.com'
+        }
+    ]
+};
+
+//----------------------------
+// Define injection function
+//----------------------------
+
+function inject(tab)
+{
+    //----------------------------
+    // Communicate with content
+    // script and ask for
+    // re-injection
+    //----------------------------
+
+    chrome.tabs.sendMessage(tab.tabId, {inject: true});
+}
+
+//----------------------------
+// DOM changed listener
+//----------------------------
+
+chrome.webNavigation.onDOMContentLoaded.addListener
+(
+    function(tab)
+    {
+        //----------------------------
+        // Re-inject button
+        //----------------------------
+
+        inject(tab);
+    },
+    filter
+);
+
+//----------------------------
+// Navigation complete listener
+//----------------------------
+
+chrome.webNavigation.onCompleted.addListener
+(
+    function(tab)
+    {
+        //----------------------------
+        // Re-inject
+        //----------------------------
+
+        inject(tab);
+    },
+    filter
+);
