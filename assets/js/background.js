078564a2fcb4bd53c586ae813031ad90367e8762
Elad Nava <eladnava@gmail.com>
2019-01-17 17:28:45 +1300

    Reformat JS code & comment syntax

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,10 +1,5 @@
-//----------------------------
-// Limit listeners to
-// https://mail.google.com/mail
-//----------------------------
-
-var filter =
-{
+// Limit listeners to https://mail.google.com/mail
+var filter = {
     'url': [
         {
             hostSuffix: 'google.com'
@@ -12,50 +7,25 @@ var filter =
     ]
 };
 
-//----------------------------
 // Define injection function
-//----------------------------
-
-function inject(tab)
-{
-    //----------------------------
-    // Communicate with content
-    // script and ask for
-    // re-injection
-    //----------------------------
-
-    chrome.tabs.sendMessage(tab.tabId, {inject: true});
+function inject(tab) {
+    // Communicate with content script and ask for re-injection
+    chrome.tabs.sendMessage(tab.tabId, { inject: true });
 }
 
-//----------------------------
 // DOM changed listener
-//----------------------------
-
-chrome.webNavigation.onDOMContentLoaded.addListener
-(
-    function(tab)
-    {
-        //----------------------------
+chrome.webNavigation.onDOMContentLoaded.addListener(
+    function (tab) {
         // Re-inject button
-        //----------------------------
-
         inject(tab);
     },
     filter
 );
 
-//----------------------------
 // Navigation complete listener
-//----------------------------
-
-chrome.webNavigation.onCompleted.addListener
-(
-    function(tab)
-    {
-        //----------------------------
+chrome.webNavigation.onCompleted.addListener(
+    function (tab) {
         // Re-inject
-        //----------------------------
-
         inject(tab);
     },
     filter
--- assets/js/content.js
+++ assets/js/content.js
@@ -1,62 +1,33 @@
-//----------------------------
-// Define compose URL
-// <to> will be replaces with
-// a CSV list of e-mails
-//----------------------------
-
+// Define compose URL <to> will be replaces with a CSV list of e-mails
 var composeURL = 'https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&bcc=<to>';
 
-//----------------------------
 // Batch reply button
-//----------------------------
-
-function getBatchReplyButton()
-{
+function getBatchReplyButton() {
     return $("div[gh='mtb'] .batch-reply");
 }
 
-//----------------------------
 // Get checked conversations
-//----------------------------
-
-function getCheckedConversations()
-{
+function getCheckedConversations() {
     return $("div[aria-checked='true']");
 }
 
-//----------------------------
 // Get conversation e-mails
-//----------------------------
-
-function getConversationEmails(message)
-{
+function getConversationEmails(message) {
     return message.parents('tr').find("span[email]");
 }
 
-//----------------------------
 // Conversations list view
-//----------------------------
-
-function getConversationsListView()
-{
+function getConversationsListView() {
     return $(".UI[gh='tl']");
 }
 
-//----------------------------
 // Conversations menu cont.
-//----------------------------
-
-function getMenuContainer()
-{
+function getMenuContainer() {
     return $("div[gh='mtb']").children(":first").children(":first");
 }
 
-//----------------------------
 // Batch reply button HTML
-//----------------------------
-
-function getBatchReplyButtonHTML()
-{
+function getBatchReplyButtonHTML() {
     return '<div class="J-J5-Ji">' +
         '<div class="batch-reply T-I T-I-ax7 batch-reply" role="button" tabindex="0" style="-webkit-user-select: none;">' +
         'Reply' +
@@ -64,211 +35,97 @@ function getBatchReplyButtonHTML()
         '</div>';
 }
 
-//----------------------------
-// Inject batch-reply button
-// (called multiple times)
-//----------------------------
-
-function refreshBatchReplyButton()
-{
-    //----------------------------
-    // Get batch reply button
-    // if already injected
-    //----------------------------
-
+// Inject batch-reply button (called multiple times)
+function refreshBatchReplyButton() {
+    // Get batch reply button if already injected
     var batchReply = getBatchReplyButton();
 
-    //----------------------------
-    // Get conversations view
-    // to check if visible
-    //----------------------------
-
+    // Get conversations view to check if visible
     var conversationsView = getConversationsListView();
 
-    //----------------------------
     // Not injected?
-    //----------------------------
-
-    if ( ! batchReply.length )
-    {
-        //----------------------------
+    if (!batchReply.length) {
         // Conversations view visible?
-        //----------------------------
-
-        if ( conversationsView.length )
-        {
-            //----------------------------
-            // Get menu container
-            // (options above list)
-            //----------------------------
-
+        if (conversationsView.length) {
+            // Get menu container (options above list)
             var menuContainer = getMenuContainer();
 
-            //----------------------------
             // Inject batch reply button
-            //----------------------------
-
             menuContainer.append(getBatchReplyButtonHTML());
 
-            //----------------------------
             // Handle batch-reply click
-            //----------------------------
-
-            getBatchReplyButton().click( function()
-            {
-                //----------------------------
+            getBatchReplyButton().click(function () {
                 // Call batch reply function
-                //----------------------------
-
                 prepareBatchReply();
             });
         }
     }
 
-    //----------------------------
     // Not in message list?
-    //----------------------------
-
-    if ( ! conversationsView.length )
-    {
-        //----------------------------
+    if (!conversationsView.length) {
         // Hide batch reply
-        //----------------------------
-
         batchReply.parent().hide();
     }
-    else
-    {
-        //----------------------------
+    else {
         // Show batch reply
-        //----------------------------
-
         batchReply.parent().show();
     }
 }
 
-function prepareBatchReply()
-{
-    //----------------------------
+function prepareBatchReply() {
     // Prepare unique emails array
-    //----------------------------
-
     var emails = [];
 
-    //----------------------------
     // Loop over checked items
-    //----------------------------
-
-    getCheckedConversations().each( function()
-    {
-        //----------------------------
+    getCheckedConversations().each(function () {
         // Get checked item
-        //----------------------------
-
         var conversation = $(this);
 
-        //----------------------------
-        // Get all e-mails related
-        // to this conversation
-        //----------------------------
-
-        getConversationEmails( conversation ).each( function()
-        {
-            //----------------------------
+        // Get all e-mails related to this conversation
+        getConversationEmails(conversation).each(function () {
             // Get email
-            //----------------------------
-
             var email = $(this);
 
-            //----------------------------
             // Is it my e-mail?
-            //----------------------------
-
-            if ( email.attr('name') == 'me' )
-            {
-                //----------------------------
+            if (email.attr('name') == 'me') {
                 // Skip it
-                //----------------------------
-
                 return;
             }
 
-            //----------------------------
             // Get e-mail address
-            //----------------------------
-
             var email = email.attr('email');
 
-            //----------------------------
             // Not already added?
-            //----------------------------
-
-            if ( emails.indexOf( email ) == -1 )
-            {
-                //----------------------------
+            if (emails.indexOf(email) == -1) {
                 // Add it to emails array
-                //----------------------------
-
-                emails.push( email );
+                emails.push(email);
             }
         });
     });
 
-    //----------------------------
     // Got anything?
-    //----------------------------
+    if (emails.length > 0) {
+        // Convert to string and URL-encode
+        emails = encodeURIComponent(emails.join(', '));
 
-    if ( emails.length > 0 )
-    {
-        //----------------------------
-        // Convert to string
-        // and URL-encode
-        //----------------------------
-
-        emails = encodeURIComponent( emails.join( ', ' ) );
-
-        //----------------------------
-        // Open it in a new window
-        // (replace <to> with emails)
-        //----------------------------
-
-        window.open( composeURL.replace( '<to>', emails ) );
+        // Open it in a new window (replace <to> with emails)
+        window.open(composeURL.replace('<to>', emails));
     }
 }
 
-//----------------------------
-// Listen for events
-// from the background script
-//----------------------------
-
-chrome.extension.onMessage.addListener
-(
-    function(request, sender, sendResponse)
-    {
-        //----------------------------
+// Listen for events from the background script
+chrome.extension.onMessage.addListener(
+    function (request, sender, sendResponse) {
         // Injection requested?
-        //----------------------------
-
-        if ( request.inject )
-        {
-            //----------------------------
+        if (request.inject) {
             // Refresh button
-            //----------------------------
-
             refreshBatchReplyButton();
         }
     }
 );
 
-//----------------------------
 // Handle HTML5 page change
-//----------------------------
-
-window.onpopstate = history.onpushstate = function(e)
-{
-    //----------------------------
+window.onpopstate = history.onpushstate = function (e) {
     // Refresh button
-    //----------------------------
-
     refreshBatchReplyButton();
 }
